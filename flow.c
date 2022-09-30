22601e4303bc364ba83fa767ab066756f4a883a4
David Chocholaty <david.chocholaty@seznam.cz>
2022-09-30 11:35:40 +0200

    Makefile: add Makefile and file structure
    The commit adds Makefile and dependent file structure.

---
+++ flow.c
@@ -0,0 +1,22 @@
+/**********************************************************/
+/*                                                        */
+/* File: flow.c                                           */
+/* Created: 2022-09-30                                    */
+/* Last change: 2022-09-30                                */
+/* Author: David Chocholaty <xchoch09@stud.fit.vutbr.cz>  */
+/* Project: Project for the course ISA - variant 1        */
+/*          - Generation of NetFlow data from captured    */
+/*            network traffic.                            */
+/* Description: NetFlow exporter                          */
+/*                                                        */
+/**********************************************************/
+
+#include <stdlib.h>
+
+/*
+ * Main function of Netflow exporter.
+ */
+int main (int argc, char *argv[])
+{
+    return EXIT_SUCCESS;
+}
