031df33288d066286a4de4cbc265b767d6dcf0bb
David Chocholaty <david.chocholaty@seznam.cz>
2022-11-13 18:15:24 +0100

    flow: refactor headers
    This commit refactors headers for the flow.c file.

--- flow.c
+++ flow.c
@@ -11,11 +11,10 @@
 
 #include "flow.h"
 
+#include <netdb.h> // For Merlin server.
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
-#include <netdb.h> // For Merlin server.
 
 #include "error.h"
 #include "memory.h"
